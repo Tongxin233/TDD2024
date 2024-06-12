2009e38158effab75dfafc9c353f518f878c96c1
zyz <910442991@qq.com>
2024-06-13 01:28:37 +0800

    message 24:move static file and database outside of main source tree!^_^

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -11,7 +11,8 @@ MAX_WAIT = 10
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
-        real_server = os.environ.get('REAL_SERVER')
+        # real_server = os.environ.get('REAL_SERVER')
+        real_server = '47.99.35.170'
         if real_server:
             self.live_server_url = 'http://'+real_server
     
--- notes/settings.py
+++ notes/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,13 +25,14 @@ SECRET_KEY = 'django-insecure-(_--+3eu%n!yfoj6xspnbw6x72zvk(eoq&xsraky7yl@+dsq@^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['47.99.35.170','localhost','127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://47.99.35.170']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'../database/db.sqlite3'),
     }
 }
 
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'../static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
